8d9afd5d34911a4556a8190253bb99c5512020a7
Cameron Napolitano <94765074+cnapolit@users.noreply.github.com>
2026-07-27 18:57:41 -0400

    Implemented LocalDataStore for etl pipeline (#26)

--- etl/src/etl/local_data_store.py
+++ etl/src/etl/local_data_store.py
@@ -1,6 +1,11 @@
 from etl.base.data_store import BaseDataStore
 from etl.dtos import NormalizedLocation
+from pathlib import Path
+import json
 
+src_key = 'source'
+loc_key = 'locations'
+data_dir = Path(__file__).parent / "data"
 
 # Reads and writes normalized locations to a local file.
 class LocalDataStore(BaseDataStore):
@@ -10,16 +15,35 @@ class LocalDataStore(BaseDataStore):
         source: str,
         normalized_locations: list[NormalizedLocation],
     ) -> None:
-        pass
+        file_path = self._get_file_path(source)
+        locations_serialized = [location.model_dump(mode="json") for location in normalized_locations]
+        snapshot = json.dumps({ src_key: source, loc_key: locations_serialized }, indent=2)
+        with open(file_path, "w") as file:
+            file.write(snapshot)
 
     def read_source_snapshot(
         self,
         source: str,
     ) -> list[NormalizedLocation]:
-        pass
+        file_path = self._get_file_path(source)
+        if not file_path.exists():
+            raise FileNotFoundError(file_path)
+
+        with open(file_path, "r") as file:
+            snapshot_serialized = json.load(file)
+
+        snapshot_source = snapshot_serialized[src_key]
+        if snapshot_source != source:
+            raise ValueError(f"Provided source '{source}' does not match file source '{snapshot_source}'")
+
+        return [NormalizedLocation.model_validate(location) for location in snapshot_serialized[loc_key]]
 
     def write_output_locations(
         self,
         output_locations: list[NormalizedLocation],
     ) -> None:
         pass
+
+    def _get_file_path(self, source: str) -> Path:
+        data_dir.mkdir(exist_ok=True)
+        return data_dir / f"{source}_snapshot.json"
